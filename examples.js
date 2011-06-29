f3010e091532421397b4cc8749f1a1da065ebca5
Tj Holowaychuk <tj@vision-media.ca>
2011-06-29 09:31:07 -0700

    fixed example

--- examples.js
+++ examples.js
@@ -6,34 +6,34 @@
 var qs = require('./');
 
 var obj = qs.parse('foo');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('foo=bar=baz');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('users[]');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('name=tj&email=tj@vision-media.ca');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('users[]=tj&users[]=tobi&users[]=jane');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('user[name][first]=tj&user[name][last]=holowaychuk');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('users[][name][first]=tj&users[][name][last]=holowaychuk');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('a=a&a=b&a=c');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('user[tj]=tj&user[tj]=TJ');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('user[names]=tj&user[names]=TJ&user[names]=Tyler');
-require('inspect')(obj)
+console.log(obj)
 
 var obj = qs.parse('user[name][first]=tj&user[name][first]=TJ');
-require('inspect')(obj)
+console.log(obj)
