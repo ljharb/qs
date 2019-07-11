b14b63845c1b8884bfd6d374e3da020c2455f0d0
Jordan Harband <ljharb@gmail.com>
2019-07-10 23:21:36 -0700

    [Dev Deps] update `eslint`, `iconv-lite`, `browserify`, `tape`

--- test/parse.js
+++ test/parse.js
@@ -617,6 +617,7 @@ test('parse()', function (t) {
     });
 
     t.test('prefers an iso-8859-1 charset specified by the utf8 sentinel to a default charset of utf-8', function (st) {
+        // eslint-disable-next-line quote-props
         st.deepEqual(qs.parse('utf8=' + urlEncodedNumCheckmark + '&' + urlEncodedOSlashInUtf8 + '=' + urlEncodedOSlashInUtf8, { charsetSentinel: true, charset: 'utf-8' }), { 'Ã¸': 'Ã¸' });
         st.end();
     });
@@ -637,6 +638,7 @@ test('parse()', function (t) {
     });
 
     t.test('uses the utf8 sentinel to switch to iso-8859-1 when no default charset is given', function (st) {
+        // eslint-disable-next-line quote-props
         st.deepEqual(qs.parse('utf8=' + urlEncodedNumCheckmark + '&' + urlEncodedOSlashInUtf8 + '=' + urlEncodedOSlashInUtf8, { charsetSentinel: true }), { 'Ã¸': 'Ã¸' });
         st.end();
     });
