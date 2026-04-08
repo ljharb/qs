6f7678505c88374a9ae668561371bf6dc3aab60d
Jordan Harband <ljharb@gmail.com>
2026-04-08 09:47:27 -0700

    [Fix] `parse`: `parameterLimit: Infinity` with `throwOnLimitExceeded: true` silently drops all parameters

--- test/parse.js
+++ test/parse.js
@@ -1153,6 +1153,12 @@ test('parse()', function (t) {
             sst.end();
         });
 
+        st.test('allows unlimited parameters when parameterLimit is Infinity and throwOnLimitExceeded is true', function (sst) {
+            var result = qs.parse('a=1&b=2&c=3&d=4&e=5&f=6', { parameterLimit: Infinity, throwOnLimitExceeded: true });
+            sst.deepEqual(result, { a: '1', b: '2', c: '3', d: '4', e: '5', f: '6' }, 'parses all parameters without truncation or throwing');
+            sst.end();
+        });
+
         st.end();
     });
 
